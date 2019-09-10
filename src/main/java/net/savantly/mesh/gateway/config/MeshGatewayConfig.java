493bcae65435d4c971ae5e7069fa8a7204a2101f
Jeremy Branham <Jeremy@Savantly.net>
2019-09-10 02:08:00 -0500

    Use new gateway-spring-boot-starter library

--- src/main/java/net/savantly/mesh/gateway/config/MeshGatewayConfig.java
+++ src/main/java/net/savantly/mesh/gateway/config/MeshGatewayConfig.java
@@ -1,22 +1,11 @@
 package net.savantly.mesh.gateway.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties("gateway")
+@ConfigurationProperties("mesh")
 public class MeshGatewayConfig {
 	
-	private Map<String, String> routes = new HashMap<>();
 
-	public Map<String, String> getRoutes() {
-		return routes;
-	}
-
-	public void setRoutes(Map<String, String> routes) {
-		this.routes = routes;
-	}
 }
